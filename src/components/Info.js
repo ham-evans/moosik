d3e8d9b2e25512fa3c116d439984797906f69690
Hamilton Evans <hamevans@Hamiltons-MBP.lan>
2021-11-11 06:14:56 -0800

    updated vision + mission

--- src/components/Info.js
+++ src/components/Info.js
@@ -12,9 +12,9 @@ export default function About () {
                         <img src={waveform3} alt="Waveform" />
                     </div>
                     <h1 id="mission">Vision</h1> 
-                    <p>Our vision to build an entire music related ecosystem connected with NFT technology. We understand this is the future of many industries, we just want to be the first to build something special and big to bring largest art industry in the world to NFT space.</p>
+                    <p>A decentralized world where privacy, security , equality and innovation are priority.</p>
                     <h1>Mission</h1>
-                    <p>Our mission is to help as many artists as possible in NFT space and make them grow with us. We will be doing talent hunts and giving opportunities to the new talent that has not yet been discovered.</p>
+                    <p>To bring Web3 and the world closer.</p>
                 </div>
             </div>  
         </div>
